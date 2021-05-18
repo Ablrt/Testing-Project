cbc197534c049f7181bfde2b2260cebad145175e
xXAldanXx <albert_amadanyan@edu.aua.am>
2021-05-18 18:25:59 +0400

    third commit

--- src/test/java/Base/BaseTest.java
+++ src/test/java/Base/BaseTest.java
@@ -1,11 +1,11 @@
 package Base;
 
 import com.google.common.io.Files;
-import org.openqa.selenium.Cookie;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
@@ -19,10 +19,16 @@ import Utils.WindowManager;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BaseTest {
     private EventFiringWebDriver driver;
+    private RemoteWebDriver chrome;
+    private RemoteWebDriver firefox;
     protected HomePage homePage;
+    Capabilities chromeCapabilities = DesiredCapabilities.chrome();
+    Capabilities firefoxCapabilities = DesiredCapabilities.firefox();
 
     @BeforeClass
     public void setUp(){
@@ -32,6 +38,11 @@ public class BaseTest {
         goHome();
         //setCookie();
     }
+    
+/*    public void remoteSetUp() throws MalformedURLException {
+        chrome = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeCapabilities);
+        firefox = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), firefoxCapabilities);
+    }*/
 
     @BeforeMethod
     public void goHome(){
